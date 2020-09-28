ec82c89a349687eab1b0675cfe0c3007aea29b5a
edmundsj <jordan.e@berkeley.edu>
2020-09-28 08:28:22 -0700

    added alignment marks for aml-bonder

---
+++ aml_bonder_marks.py
@@ -0,0 +1,67 @@
+import numpy as np
+import gdspy as gdspy
+from testStructures import *
+from cellFunctions import *
+
+# The GDSII file is called a library, which contains multiple cells.
+lib = gdspy.GdsLibrary()
+
+# Units are in microns
+mm = 1e3
+
+# Geometry must be placed in cells.
+cell = lib.new_cell('FIRST')
+markOffset = 58*mm
+
+
+topMarkLeft = gdspy.Polygon([(-10, 10), (-10, 50), (10, 50), (10, 10), (50, 10), (50, -10), \
+        (10, -10), (10, -50), (-10, -50), (-10, -10), (-50, -10), (-50, 10), (-10, 10)])
+topMarkRight = gdspy.Polygon([(-10, 10), (-10, 50), (10, 50), (10, 10), (50, 10), (50, -10), \
+        (10, -10), (10, -50), (-10, -50), (-10, -10), (-50, -10), (-50, 10), (-10, 10)])
+topMarkLeft.translate(-markOffset,0)
+topMarkRight.translate(markOffset,0)
+keepoutLeft = gdspy.Rectangle((-5*mm, -5*mm), (5*mm, 5*mm))
+keepoutRight = gdspy.Rectangle((-5*mm, -5*mm), (5*mm, 5*mm))
+keepoutLeft.translate(-markOffset, 0)
+keepoutRight.translate(markOffset, 0)
+
+totalLeft = gdspy.boolean(keepoutLeft, topMarkLeft, 'not')
+totalRight = gdspy.boolean(keepoutRight, topMarkRight, 'not')
+
+cell.add(totalLeft)
+cell.add(totalRight)
+
+# SAVE THE FILE
+lib.write_gds('aml_top_marks.gds')
+
+# Display all cells using the internal viewer.
+#gdspy.LayoutViewer(lib)
+
+bottomLib = gdspy.GdsLibrary()
+bottomCell = bottomLib.new_cell('TOP')
+print(bottomLib)
+
+squareOffset = 10+5+35/2
+bottomSquare = gdspy.Rectangle((-35/2, -35/2), (35/2, 35/2), layer=2)
+bottomSquareTR = gdspy.copy(bottomSquare)
+bottomSquareTR.translate(squareOffset, squareOffset)
+bottomSquareTL = gdspy.copy(bottomSquare)
+bottomSquareTL.translate(-squareOffset, squareOffset)
+bottomSquareBR = gdspy.copy(bottomSquare)
+bottomSquareBR.translate(squareOffset, -squareOffset)
+bottomSquareBL = gdspy.copy(bottomSquare)
+bottomSquareBL.translate(-squareOffset, -squareOffset)
+bottomMark = gdspy.Cell('Bottom Mark')
+
+bottomMark.add(bottomSquareTR)
+bottomMark.add(bottomSquareTL)
+bottomMark.add(bottomSquareBR)
+bottomMark.add(bottomSquareBL)
+
+bottomMarkLeft = gdspy.CellReference(bottomMark, (-markOffset, 0))
+bottomMarkRight = gdspy.CellReference(bottomMark, (markOffset, 0))
+
+cell.add(bottomMarkLeft)
+cell.add(bottomMarkRight)
+
+gdspy.LayoutViewer(lib)
