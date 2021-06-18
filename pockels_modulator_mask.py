643763ade7a05c17c530cc7c617981650a3192c2
edmundsj <jordan.e@berkeley.edu>
2021-06-18 10:07:29 -0700

    added RC modeling and updated mask for new high-Q high-V process

--- pockels_modulator_mask.py
+++ pockels_modulator_mask.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gdspy as gdspy
+from itertools import product
 from testStructures import *
 from cellFunctions import *
 from ASMLBarcodeGenerator import ASMLBarcodeGenerator
@@ -10,29 +11,55 @@ lib = gdspy.GdsLibrary()
 # Geometry must be placed in cells.
 cell = lib.new_cell('FIRST')
 
-# Create the geometry (a single rectangle) and add it to the cell.
-
 mm=1000
 Lgap = 50
-traceWidth = 25
-traceSpacing = 10
-Rdevice = 1000
-multiplier = 0.7
-ringWidth = 750 * np.array([np.power(multiplier, 1), np.power(multiplier, 2), np.power(multiplier, 3),
-        np.power(multiplier, 4), np.power(multiplier, 5), np.power(multiplier,6), np.power(multiplier, 7)])
-absoluteOrigin = np.array([0,0])
-dieSize = 10*mm
+trace_width = 2 # Width of traces
+trace_spacing = 1 # Distance from trace to nearest feature
 magnificationASML = 4
-fieldSize = dieSize * magnificationASML
+die_length = 5*mm
+fieldSize = die_length * magnificationASML
 fieldSpacing = 4.8*mm # minimum allowed ASML field spacing
-bondPadSize = 150
+text_size = 20 # size of text used on most of the die
+die_padding = 400 # amount of space from the edge of the die to important features
+pad_size = 120 # bond pad length and width
+pad_spacing = 10 # spacing between bond pad duplicates
 
-device = gdspy.Round(absoluteOrigin, Rdevice)
-cell.add(device)
+absoluteOrigin = np.array([0,0])
 
-dieBoundary = gdspy.Rectangle([-dieSize/2, -dieSize/2], [dieSize/2, dieSize/2], layer=1)
-dieBoundaryInner = gdspy.Rectangle([-dieSize/2 + traceWidth, -dieSize/2 + traceWidth],
-        [dieSize/2 - traceWidth, dieSize/2 - traceWidth], layer=1)
+### BEGIN DEVICE STRUCTURES
+
+device_diameters = [5, 10, 20, 40, 80, 160, 320, 640, 1280]
+device_spacing = 1*mm
+device_coordinates = [np.array(x)*device_spacing for x in \
+                     [(1,1), (0,1),(-1,1),
+                       (1,0),(0,0),(-1,0),
+                       (-1,-1),(0,-1),(1,-1)]]
+# ensure the contacts only take up 1% of device area
+ring_contact_spacing = 4 * trace_spacing
+ring_contact_width = 4 * trace_width
+devices = [gdspy.Round(center, diameter/2) \
+        for center, diameter in zip(device_coordinates, device_diameters)]
+ito_preserve = [gdspy.Round(
+        center,
+        diameter/2 + ring_contact_width + ring_contact_spacing,
+        layer=1) \
+        for center, diameter in zip(device_coordinates, device_diameters)]
+ring_contacts = [gdspy.Round(
+        center,
+        diameter/2 + ring_contact_width + ring_contact_spacing,
+        inner_radius= diameter/2 + ring_contact_spacing,
+        layer=2) \
+        for center, diameter in zip(device_coordinates, device_diameters)]
+
+for r, i, d in zip(ring_contacts, ito_preserve, devices):
+    cell.add(d)
+    cell.add(i)
+    cell.add(r)
+
+
+dieBoundary = gdspy.Rectangle([-die_length/2, -die_length/2], [die_length/2, die_length/2], layer=1)
+dieBoundaryInner = gdspy.Rectangle([-die_length/2 + trace_width, -die_length/2 + trace_width],
+        [die_length/2 - trace_width, die_length/2 - trace_width], layer=1)
 dieBoundary0 = gdspy.boolean(dieBoundary, dieBoundaryInner, 'not', layer=0)
 dieBoundary1 = gdspy.boolean(dieBoundary, dieBoundaryInner, 'not', layer=1)
 dieBoundary2 = gdspy.boolean(dieBoundary, dieBoundaryInner, 'not', layer=2)
@@ -40,88 +67,181 @@ cell.add(dieBoundary0)
 cell.add(dieBoundary1)
 cell.add(dieBoundary2)
 
-cell.add(generateRings(lib, centerWidth=Rdevice*2+Lgap*2, centerHeight=Rdevice*2+Lgap*2,
-    traceWidth=traceWidth, traceSpacing=traceSpacing, ringWidth=ringWidth))
+def bond_pad_group(offset=[0,0], pad_spacing=50, layer=2):
 
-deviceToPadTrace = gdspy.Rectangle([-Rdevice + traceSpacing + traceWidth, traceWidth/2],
-        [-dieSize/2 + traceSpacing + traceWidth, -traceWidth/2], layer=1)
-cell.add(deviceToPadTrace)
+    bond_pad_center = gdspy.Rectangle(
+        [offset[0],
+            offset[1] - pad_size/2],
+        [offset[0] + pad_size,
+            offset[1] + pad_size/2],
+            layer=layer)
 
-deviceRingContact = gdspy.Round((0, 0), Rdevice - traceSpacing, inner_radius=(Rdevice - traceSpacing - traceWidth), layer=1)
-cell.add(deviceRingContact)
+    bond_pad_top = gdspy.Rectangle(
+        [offset[0],
+            offset[1] - pad_size/2 - pad_spacing - pad_size],
+        [offset[0] + pad_size,
+            offset[1] + pad_size/2 - pad_spacing - pad_size],
+            layer=layer)
 
-bondPad = gdspy.Rectangle([-dieSize/2 + traceWidth + traceSpacing, -bondPadSize/2],
-        [-dieSize/2 + traceWidth + traceSpacing + bondPadSize, bondPadSize/2], layer=1)
-cell.add(bondPad)
+    bond_pad_bottom = gdspy.Rectangle(
+        [offset[0],
+            offset[1] - pad_size/2 + pad_spacing + pad_size],
+        [offset[0] + pad_size,
+            offset[1] + pad_size/2 + pad_spacing + pad_size],
+        layer=layer)
+
+    connector_trace = gdspy.Rectangle(
+        [offset[0] + pad_size*2 + trace_spacing,
+            offset[1] - pad_spacing - pad_size/2 - pad_size],
+        [offset[0] + pad_size*2 + trace_spacing + trace_width,
+            offset[1] + pad_size/2 + pad_spacing + pad_size],
+        layer=layer)
+
+    connector_top = gdspy.Rectangle(
+        [offset[0] + pad_size,
+            offset[1] + pad_spacing + pad_size/2 + pad_size/2],
+        [offset[0] + pad_size*2 + trace_spacing + trace_width,
+            offset[1] + pad_spacing + pad_size/2 + pad_size/2 + \
+            trace_width],
+        layer=layer)
+
+    connector_middle = gdspy.Rectangle(
+        [offset[0] + pad_size,
+            offset[1]],
+        [offset[0] + pad_size*2 + trace_width,
+            offset[1] + trace_width],
+        layer=layer)
+
+    connector_bottom = gdspy.Rectangle(
+        [offset[0] + pad_size,
+            offset[1] - pad_spacing - pad_size/2 - pad_size/2],
+        [offset[0] + pad_size*2 + trace_width,
+            offset[1] - pad_spacing - pad_size/2 - pad_size/2 + trace_width],
+        layer=layer)
+
+    full_polygon_1 = gdspy.boolean(bond_pad_center, bond_pad_top,
+            "or", layer=layer)
+    full_polygon_2 = gdspy.boolean(bond_pad_bottom, connector_trace,
+            "or", layer=layer)
+    connector_polygon_1 = gdspy.boolean(connector_top, connector_bottom,
+            "or", layer=layer)
+    connector_polygon_2 = gdspy.boolean(connector_middle, connector_trace,
+            "or", layer=layer)
+
+    connector_polygon = gdspy.boolean(connector_polygon_1,
+            connector_polygon_2,
+            "or", layer=layer)
+    full_polygon = gdspy.boolean(full_polygon_1, full_polygon_2,
+            "or", layer=layer)
+
+    full_polygon = gdspy.boolean(full_polygon, connector_polygon,
+            "or", layer=layer)
+    return full_polygon
+
+pad_locations = [
+    [-die_length/2 + die_padding,
+    (die_length - die_padding * 2 - pad_size*3 - pad_spacing*2)/2*(1-x/4)] \
+                for x in range(9)]
+text_locations = [[x[0] + pad_size + trace_spacing,
+               x[1] + trace_width + trace_spacing] \
+                for x in pad_locations]
+bond_groups = [bond_pad_group(offset=loc, pad_spacing=pad_spacing ) \
+              for loc in pad_locations]
+bond_text = [str(diam) + 'u' for diam in device_diameters]
+bond_text_gds = [gdspy.Text(t, text_size, offset, layer=2) \
+    for t, offset in zip(bond_text, text_locations)]
+for text, group in zip(bond_text_gds, bond_groups):
+    cell.add(group)
+    cell.add(text)
+
+# Routing traces
+for i in range(9):
+    distance_from_center = device_diameters[i]/2 + ring_contact_spacing
+    if i < 5:
+        trace_start_location = device_coordinates[i] + distance_from_center/ np.sqrt(2) * np.array([-1,1])
+    else:
+        trace_start_location = device_coordinates[i] + distance_from_center / np.sqrt(2) * np.array([-1, -1])
+
+    trace_delta_vector = abs(pad_locations[i] - trace_start_location)
+    trace_delta_height = trace_delta_vector[1]
+    trace_delta_width = trace_delta_vector[0] - trace_delta_height
+    trace_final_location = pad_locations[i]
+    trace = gdspy.Path(trace_width, trace_start_location)
+    if i < 3:
+        trace.segment(trace_delta_height * np.sqrt(2), direction=3/4*np.pi, layer=2)
+        trace.turn(trace_width, 1/4*np.pi, layer=2)
+        trace.segment(trace_delta_width, direction='-x', layer=2)
+    elif i == 3:
+        trace.segment(trace_delta_height*2/3 * np.sqrt(2), direction=3/4*np.pi, layer=2)
+        trace.turn(trace_width, 1/4*np.pi, layer=2)
+        trace.segment(trace_delta_width/3, direction='-x', layer=2)
+        trace.turn(trace_width, -1/4*np.pi, layer=2)
+        trace.segment(trace_delta_height*1/3 * np.sqrt(2), direction=3/4*np.pi, layer=2)
+        trace.turn(trace_width, 1/4*np.pi, layer=2)
+        trace.segment(trace_delta_width*2/3, direction='-x', layer=2)
+    elif i == 4:
+        trace.segment(1/3*mm * np.sqrt(2), direction=3/4*np.pi, layer=2)
+        trace.turn(trace_width, 1/4*np.pi, layer=2)
+        trace.segment(trace_delta_width*2/3 - 2/3*mm, direction='-x', layer=2)
+        trace.turn(trace_width, -1/4*np.pi, layer=2)
+        trace.segment((trace_delta_height + 1/3*mm)*np.sqrt(2), direction=-3/4*np.pi, layer=2)
+        trace.turn(trace_width, 1/4*np.pi, layer=2)
+        trace.segment(trace_delta_width*1/3, direction='-x', layer=2)
+    elif i >= 5:
+        trace.segment(trace_delta_height * np.sqrt(2), direction=-3/4*np.pi, layer=2)
+        trace.turn(trace_width, 1/4*np.pi, layer=2)
+        trace.segment(trace_delta_width, direction='-x', layer=2)
+
+    cell.add(trace)
+
+### END DEVICE STRUCTURES ###
+
+### BEGIN TEST STRUCTURES ###
+
+# ELECTRICAL TEST STRUCTURES - capacitor and contact resistance
+cell.add(addCapacitorStructures(lib, width=[200,400],
+            offset=[1700, die_length/2 - die_padding-1000], name="CAPS1"))
+cell.add(addCapacitorStructures(lib, width=[200,400],
+            offset=[1700, die_length/2 - die_padding -1500], name="CAPS2"))
+cell.add(addCapacitorStructures(lib, width=[200,400],
+            offset=[1700, die_length/2 - die_padding -1500], name="CAPS3", layer=1))
+cell.add(addTLMStructures(lib, offset=[500, die_length/2 - die_padding]))
+cell.add(addTLMStructures(lib, offset=[500, die_length/2 - die_padding - 200], name="TLM2", width=200))
+
+# LITHOGRAPHIC TEST STRUCTURES - lines
+test_widths = [0.5, 2, 5, 10]
+test_lines = [addTestLines(lib,
+        offset=[-1700 + 300*i, die_length/2-50],
+        width=x,
+        name='LINES' + str(x),
+        layer=0) \
+        for i, x in enumerate(test_widths)]
+test_line_cell = lib.new_cell('LINES')
+for line in test_lines:
+    test_line_cell.add(line)
+cell.add(test_line_cell)
 
 
-# DONE - Resistor test structures
-# 1. Contact resistance test structures
-# 2. Sheet resistance test structures
-# Both these can be done using transmission line structures. These allow us to determine the transfer length, the sheet resistance, and the specific contact resistance. This will only work when we can pattern the underlying semiconductor, which is not the case with the substrate.
-
-cell.add(addCapacitorStructures(lib, offset=[2000, dieSize/2 - traceWidth - traceSpacing]))
-cell.add(addTLMStructures(lib, offset=[0, dieSize/2 - traceWidth - traceSpacing]))
-cell.add(addTLMStructures(lib, offset=[0, dieSize/2 - traceWidth - traceSpacing - 200], name="TLM2", width=200))
-
-# DONE - Line (exposure) test structures
-testLines1 = addTestLines(lib, offset=[-2000, dieSize/2 - traceWidth - traceSpacing], width=0.5, name="LINES0.5")
-testLines2 = addTestLines(lib, offset=[-1700, dieSize/2 - traceWidth - traceSpacing], width=2, name="LINES2")
-testLines3 = addTestLines(lib, offset=[-1400, dieSize/2 - traceWidth - traceSpacing], width=5, name="LINES5")
-testLines4 = addTestLines(lib, offset=[-1100, dieSize/2 - traceWidth - traceSpacing], width=10, name="LINES10")
-testLines5 = addTestLines(lib, offset=[-800, dieSize/2 - traceWidth - traceSpacing], width=20, name="LINES20")
-testLines = lib.new_cell('LINES')
-testLines.add(testLines1)
-testLines.add(testLines2)
-testLines.add(testLines3)
-testLines.add(testLines4)
-testLines.add(testLines5)
-cell.add(testLines)
-
-cell.add(addTestLines(lib, offset=[-2000, dieSize/2 - traceWidth - traceSpacing-400], width=0.5,
+cell.add(addTestLines(lib, offset=[-200, die_length/2-50], width=0.5,
     name="LINES0.5_1", layer=1))
-cell.add(addTestLines(lib, offset=[-1700, dieSize/2 - traceWidth - traceSpacing - 400], width=2,
+cell.add(addTestLines(lib, offset=[100, die_length/2-50], width=2,
     name="LINES2_1", layer=1))
-cell.add(addTestLines(lib, offset=[-1400, dieSize/2 - traceWidth - traceSpacing - 400], width=5,
+cell.add(addTestLines(lib, offset=[400, die_length/2-50], width=5,
     name="LINES5_1", layer=1))
-cell.add(addTestLines(lib, offset=[-1100, dieSize/2 - traceWidth - traceSpacing - 400], width=10,
+cell.add(addTestLines(lib, offset=[700, die_length/2-50], width=10,
     name="LINES10_1", layer=1))
-cell.add(addTestLines(lib, offset=[-800, dieSize/2 - traceWidth - traceSpacing - 400], width=20,
-    name="LINES20_1", layer=1))
 
-cell.add(addTestLines(lib, offset=[-2000, dieSize/2 - traceWidth - traceSpacing], width=0.5,
+cell.add(addTestLines(lib, offset=[1000, die_length/2-50], width=0.5,
     name="LINES0.5_2", layer=2))
-cell.add(addTestLines(lib, offset=[-1700, dieSize/2 - traceWidth - traceSpacing], width=2,
+cell.add(addTestLines(lib, offset=[1300, die_length/2-50], width=2,
     name="LINES2_2", layer=2))
-cell.add(addTestLines(lib, offset=[-1400, dieSize/2 - traceWidth - traceSpacing], width=5,
+cell.add(addTestLines(lib, offset=[1600, die_length/2 - 50], width=5,
     name="LINES5_2", layer=2))
-cell.add(addTestLines(lib, offset=[-1100, dieSize/2 - traceWidth - traceSpacing], width=10,
+cell.add(addTestLines(lib, offset=[1900, die_length/2 - 50], width=10,
     name="LINES10_2", layer=2))
-cell.add(addTestLines(lib, offset=[-800, dieSize/2 - traceWidth - traceSpacing], width=20,
-    name="LINES20_2", layer=2))
 
-# TODO - Focusing pattern (layer 3)
-focusingRings = generateRings(lib, centerWidth=4*Rdevice+8*Lgap, offset=[Rdevice+3*Lgap, 0], centerHeight=Rdevice*2+Lgap*2,
-    layer=2, name="RINGS_FOCUS", ringWidth=ringWidth)
-cell.add(focusingRings)
+lib.write_gds('jepckmod2_core.gds')
 
-firstMirror = gdspy.Round([0, 0], Rdevice, layer=2)
-focusingMirror = lib.new_cell('focusing_mirror')
-focusingMirror.add(firstMirror)
-focusingMirror2 = gdspy.CellReference(focusingMirror, [Rdevice*(1+1/2)+Lgap, 0], magnification=1/2.0)
-focusingMirror3 = gdspy.CellReference(focusingMirror, [Rdevice*(1+2*1/2+1/4)+2*Lgap, 0], magnification=1/4.0)
-focusingMirror4 = gdspy.CellReference(focusingMirror, [Rdevice*(1+2*1/2+2*1/4+1/8)+3*Lgap, 0], magnification=1/8.0)
-focusingMirror5 = gdspy.CellReference(focusingMirror, [Rdevice*(1+2*1/2+2*1/4+2*1/8+1/16)+4*Lgap, 0],
-        magnification=1/16.0)
-focusingMirror6 = gdspy.CellReference(focusingMirror, [Rdevice*(1+2*1/2+2*1/4+2*1/8+2*1/16+1/32)+5*Lgap, 0],
-        magnification=1/32.0)
-focusingMirror7 = gdspy.CellReference(focusingMirror, [Rdevice*(1+2*1/2+2*1/4+2*1/8+2*1/16+2*1/32+1/64)+6*Lgap, 0],
-        magnification=1/64.0)
-
-cell.add(focusingMirror)
-cell.add([focusingMirror2, focusingMirror3, focusingMirror4, focusingMirror5, focusingMirror6, focusingMirror7])
-
-# Change polarity of each layer as necessary
 myPolygons = cell.get_polygons(by_spec=True)
 layer0Coordinates = myPolygons[(0, 0)]
 layer1Coordinates = myPolygons[(1, 0)]
@@ -148,7 +268,7 @@ for coords in layer0Coordinates:
 # Invert the mask for layer 0
 layer0Mask =  gdspy.Rectangle([-fieldSpacing/2, fieldSpacing/2],
         [-fieldSpacing/2 - fieldSize, fieldSpacing/2 + fieldSize])
-layer0Cell = gdspy.boolean(layer0Mask, layer0Cell, 'not', layer=4)
+#layer0Cell = gdspy.boolean(layer0Mask, layer0Cell, 'not', layer=4)
 
 for coords in layer1Coordinates:
     tempPolygon = gdspy.Polygon(coords, layer=4)
@@ -176,13 +296,13 @@ reticleCell.add(layer1Cell)
 reticleCell.add(layer2Cell)
 reticleCell.add(layer4Cell)
 
-# ADD THE BARCODE AND BARCODE TEXT
-barcodeCell, textCell = ASMLBarcodeGenerator.generateBarcodeAndText('JEPCKMOD1')
+# BARCODE AND BARCODE TEXT
+barcodeCell, textCell = ASMLBarcodeGenerator.generateBarcodeAndText('JEPCKMOD2')
 reticleCell.add(barcodeCell)
 reticleCell.add(textCell)
 
 # SAVE THE FILE
-reticleLibrary.write_gds('jepckmod1.gds')
+reticleLibrary.write_gds('jepckmod2.gds')
 
 # Display all cells using the internal viewer.
-gdspy.LayoutViewer(reticleLibrary)
+#gdspy.LayoutViewer(reticleLibrary)
